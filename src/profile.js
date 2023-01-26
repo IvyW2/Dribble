373ff47e2f3354b50925306113aeb798128ef77b
IvyW2 <ivywambugha2@gmail.com>
2023-01-26 18:28:46 +0300

    Add useState in Profile.js

---
+++ src/profile.js
@@ -0,0 +1,8 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function DribbbleProfile() {
+    const [user, setUser] = useState([]);
+    const [shots, setShots] = useState([]);
+    const [update, setUpdates]= useState([]);
+}
