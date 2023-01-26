0643a364e6163ae080b96f9e71509f75bf98bfb8
Musyoki-Wambua <wjosephmusyoki@gmail.com>
2023-01-26 23:24:28 +0300

    Basic Core Functionality

--- src/component/Project.js
+++ src/component/Project.js
@@ -2,4 +2,47 @@ import React from "react";
 
 function Projects () {
 
+    const [ name, setName] = useState('');
+  const [ description, setDescription] = useState('');
+
+  useEffect(() => {
+    console.log(isEdit, activeProject);
+    isEdit ? setName(activeProject?.name) : setName('')
+    isEdit ? setDescription(activeProject?.description) : setDescription('')
+  }, [isEdit, activeProject])
+
+  
+  const addProject = () => {
+    const payload = {
+       name,
+       description
+    }
+
+    axiosApi.post(PROJECTS.add, payload).then(resp=>{
+        toggleModal();
+        listProjects()
+    })
+  }
+
+
+
+  return (
+    <div>      
+      <Modal>
+        <Modal.Header closeButton>
+          <Modal.Title>Add/Edit Project</Modal.Title>
+        </Modal.Header>
+        <Modal.Footer>
+          <Button variant="secondary" >
+            Close
+          </Button>
+          <Button variant="primary" onClick={() => isEdit ? editProject() :  addProject()}>
+           {isEdit ? 'Edit' : 'Add'}
+          </Button>
+        </Modal.Footer>
+      </Modal>  
+    </div>
+  )
+    
+
 }export default Projects;
