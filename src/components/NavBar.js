12ecdd16131bf03dc63c90c6c84c94c2e21cd9a8
tony555t <tonytullah764@gmail.com>
2023-01-26 19:09:58 +0300

    import

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,28 @@
-import React from "react";
-function Navbar(){
-    return(
 
-    )
-    
+import Nav from 'react-bootstrap/Nav';
+import { Form} from 'react-bootstrap';
+import Navbar from 'react-bootstrap/Navbar';
+import LoginModal from './Login';
 
-}
-export default Navbar;
+// function NavbarCustom() {
+//   return (
+//     <Navbar className='nav' expand="lg">
+//         <Navbar.Brand className='app' href="#home">Dribble</Navbar.Brand>
+//         <Navbar.Toggle aria-controls="basic-navbar-nav" />
+//         <Navbar.Collapse id="basic-navbar-nav">
+//           <Nav className="me-auto">
+//             <Nav.Link className='navitem' href="/">Home</Nav.Link>
+//             <Nav.Link className='navitem' href="/profile">Profile</Nav.Link>
+//             <Nav.Link className='navitem' href="/projects">Projects</Nav.Link>
+            
+//           </Nav>
+//           <Form className="d-flex">
+            
+//             <LoginModal />
+//           </Form>
+//         </Navbar.Collapse>
+//     </Navbar>
+//   );
+// }
+
+// export default NavbarCustom;
