22f640d551729f192f8cc1867e3b667cd835c654
IvyW2 <ivywambugha2@gmail.com>
2023-01-27 04:49:42 +0300

    Add routes to App.js file

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import {Routes, Route}  from 'react-router-dom';
+import '../App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Landing from './Landing';
+import NavbarCustom from './Navbar';
+
+import Profile from './profile';
+import ProjectItem from './ProjectItem';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='body'>
+      <NavbarCustom/>
+      <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/projects" element={<ProjectItem />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
     </div>
   );
 }
 
 export default App;
+
