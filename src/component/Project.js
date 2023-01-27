0636f2c4a69bc149161d2656091d204431a203a8
Musyoki-Wambua <wjosephmusyoki@gmail.com>
2023-01-27 04:50:03 +0300

    Add, Post adn Edit Project

--- src/component/Project.js
+++ src/component/Project.js
@@ -1,8 +1,13 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Modal from 'react-bootstrap/Modal';
+import { axiosApi } from '../api/base';
+import { PROJECTS } from '../api/endpoints';
 
-function Projects () {
-
-    const [ name, setName] = useState('');
+function Project({toggleModal, activeProject , isEdit, listProjects}) {
+  
+  const [ name, setName] = useState('');
   const [ description, setDescription] = useState('');
 
   useEffect(() => {
@@ -11,7 +16,6 @@ function Projects () {
     isEdit ? setDescription(activeProject?.description) : setDescription('')
   }, [isEdit, activeProject])
 
-  
   const addProject = () => {
     const payload = {
        name,
@@ -24,7 +28,17 @@ function Projects () {
     })
   }
 
+  const editProject = () => {
+    const payload = {
+       name,
+       description
+    }
 
+    axiosApi.put(PROJECTS.edit(activeProject?.id), payload).then(resp=>{
+        toggleModal();
+        listProjects()
+    })
+  } 
 
   return (
     <div>      
@@ -45,4 +59,4 @@ function Projects () {
   )
     
 
-}export default Projects;
+}export default Project;
